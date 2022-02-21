81561722ba1bef002dc496794a4f7c0e041c6aad
GRP12BSE1 <97882984+GRP12BSE1@users.noreply.github.com>
2022-02-21 15:51:35 +0300

    Add files via upload

---
+++ chapter 5/src/exercise 1.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        a = []
+        while True:
+           N = input("Enter number: ")
+           if N == "done".lower():
+            break
+           N = a.append(float(N))
+
+    except:
+            print("INVALID INPUT")
+            continue
+    S = sum(a)
+    A = sum(a)/len(a)
+    C = len(a)
+    print(S, A, C)
+    break
